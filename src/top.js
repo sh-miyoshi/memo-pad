c722f6ad68fc58452e95e293faaf2d2661c5c885
s.miyoshi <s.miyoshi1991@gmail.com>
2021-08-03 22:43:49 +0900

    create add button

--- src/top.js
+++ src/top.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import { Text, View } from 'react-native';
-import { Header, Icon, Overlay } from 'react-native-elements';
+import { Header, Icon, Overlay, Button } from 'react-native-elements';
 
 export const Top = () => {
   return (
@@ -21,12 +21,25 @@ const TopHeader = () => {
     <View>
       <Header
         centerComponent={{ text: 'メモ帳', style: { color: '#ffffff' } }}
-        rightComponent={<Icon name='menu' color='#ffffff' onPress={toggleMenu} />}
+        rightComponent={<Icon name='menu' type="feather" color='#ffffff' onPress={toggleMenu} />}
         containerStyle={{ backgroundColor: '#808080' }}
       />
       <Overlay isVisible={menuShow} onBackdropPress={toggleMenu}>
         <Text>設定</Text>
       </Overlay>
+
+      <Button
+        icon={
+          <Icon
+            name="pluscircleo"
+            type="antdesign"
+            size={15}
+            color="white"
+          />
+        }
+        title="新しいメモを追加"
+      />
+
     </View>
   )
 }
