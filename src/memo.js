4a300a33351c23040c78c147c05c54811456830a
s.miyoshi <s.miyoshi1991@gmail.com>
2021-08-06 22:58:34 +0900

    update memo info page view

--- src/memo.js
+++ src/memo.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import { View, StyleSheet } from 'react-native';
-import { Header } from 'react-native-elements';
+import { Header, Icon } from 'react-native-elements';
+import { TextInput, DefaultTheme } from 'react-native-paper';
 
 export const Memo = () => {
   return (
     <View style={styles.container}>
       <MemoHeader />
+      <MemoBody />
     </View>
   )
 }
@@ -14,8 +16,29 @@ const MemoHeader = () => {
   return (
     <View>
       <Header
-        rightComponent={{ text: '削除アイコン、メニュー' }}
+        rightComponent={
+          <View style={styles.headerIcon}>
+            <Icon name='delete' type="antdesign" color='#ffffff' />
+            <Icon name='menu' type="feather" color='#ffffff' style={{ marginLeft: 16 }} />
+          </View>
+        }
         containerStyle={{ backgroundColor: '#808080' }}
+      />
+    </View>
+  )
+}
+
+const MemoBody = () => {
+  const [text, setText] = React.useState('');
+
+  return (
+    <View style={styles.textinput}>
+      <TextInput
+        value={text}
+        onChangeText={text => setText(text)}
+        multiline
+        placeholder="メモ内容(自動保存されます)"
+        theme={textTheme}
       />
     </View>
   )
@@ -27,4 +50,20 @@ const styles = StyleSheet.create({
     backgroundColor: '#fffafa',
     alignItems: 'center',
   },
+
+  headerIcon: {
+    flexDirection: 'row'
+  },
+
+  textinput: {
+    width: '90%'
+  },
 });
+
+const textTheme = {
+  ...DefaultTheme,
+  colors: {
+    primary: '#000000',
+    background: '#fafafa'
+  }
+}
